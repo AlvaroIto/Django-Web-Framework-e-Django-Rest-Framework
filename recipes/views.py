1257440db61152d633639ad57fdce66486edae17
Alvaro Ito <alvaro.e.ito@gmail.com>
2025-04-09 10:25:10 -0300

    att

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
+from django.http import Http404
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
@@ -11,8 +11,13 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+
+    if not recipes:
+        raise Http404('Not found')
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category | '
     })
 
 def recipe(request, id):
