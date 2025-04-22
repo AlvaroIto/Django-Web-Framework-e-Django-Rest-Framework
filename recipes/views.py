4598cff31840cc8ea9e6c894088ba61058259a6b
Alvaro Ito <alvaro.e.ito@gmail.com>
2025-04-22 16:14:33 -0300

    Iniciado seção 10

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.http import Http404
 from recipes.models import Recipe
 
 def home(request):
@@ -23,4 +24,12 @@ def recipe(request, id):
         'recipe': recipe,
         'is_detail_page': True,
     })
+
+def search(request):
+    search_term = request.GET.get('q')
+
+    if not search_term:
+        raise Http404()
+
+    return render(request, 'recipes/pages/search.html')
 
