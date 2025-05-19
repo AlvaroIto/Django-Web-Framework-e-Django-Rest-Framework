f96fd919509d586a19ea50814ab0ae1ccaecf1bd
Alvaro Ito <alvaro.e.ito@gmail.com>
2025-05-19 09:30:36 -0300

    att

--- authors/forms.py
+++ authors/forms.py
@@ -1,7 +1,37 @@
 from django import forms
 from django.contrib.auth.models import User
 
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
+
+def add_placeholder(field, placeholder_val):
+    add_attr(field, 'placeholder', placeholder_val)
+
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['first_name'], 'Enter your first name')
+        add_placeholder(self.fields['last_name'], 'Enter your last name')
+        add_placeholder(self.fields['username'], 'Enter your username')
+        add_placeholder(self.fields['email'], 'Enter your email address')
+    
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Your password',
+        })
+    )
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Confirm your password',
+        })
+    )
+
+
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password']
@@ -30,3 +60,8 @@ class RegisterForm(forms.ModelForm):
             })
             
         }
+
+    def clean_password(self):
+        data = self.cleaned_data.get('password')
+        
+        return data
